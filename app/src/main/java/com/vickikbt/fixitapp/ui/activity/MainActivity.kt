4282514eb0d0f703631930a8cc1c37b347a8e51f
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2021-01-10 16:04:03 +0300

    Updated nav header profile in MainActivity

--- app/src/main/java/com/vickikbt/fixitapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/vickikbt/fixitapp/ui/activity/MainActivity.kt
@@ -2,6 +2,9 @@ package com.vickikbt.fixitapp.ui.activity
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
@@ -10,15 +13,21 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.vickikbt.fixitapp.R
 import com.vickikbt.fixitapp.databinding.ActivityMainBinding
+import com.vickikbt.fixitapp.ui.viewmodels.UserViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import jp.wasabeef.glide.transformations.BlurTransformation
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private val viewModel by viewModels<UserViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,6 +56,34 @@ class MainActivity : AppCompatActivity() {
                 toolbar.visibility = View.GONE
             } else toolbar.visibility = View.VISIBLE
         }
+
+        initUI()
+    }
+
+    private fun initUI() {
+        val headerUsername: TextView =
+            binding.navView.getHeaderView(0).findViewById(R.id.userName_navHeader)
+        val headerEmail: TextView =
+            binding.navView.getHeaderView(0).findViewById(R.id.emailAddress_navHeader)
+        val headerImageView: ImageView =
+            binding.navView.getHeaderView(0).findViewById(R.id.imageView_navHeader)
+        val headerImageViewBackground: ImageView =
+            binding.navView.getHeaderView(0).findViewById(R.id.imageView_navHeaderBackground)
+
+        viewModel.getCurrentUser.observe(this, {user->
+            if (user!=null){
+                headerUsername.text = user.username
+                headerEmail.text = user.email
+                Glide.with(this)
+                    .load(user.imageUrl)
+                    .placeholder(R.drawable.ic_profile_pic)
+                    .into(headerImageView)
+
+                Glide.with(this).load(user.imageUrl)
+                    .apply(RequestOptions.bitmapTransform(BlurTransformation(25, 3)))
+                    .into(headerImageViewBackground)
+            }
+        })
     }
 
     override fun onSupportNavigateUp(): Boolean {
