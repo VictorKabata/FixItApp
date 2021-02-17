ea742d15dd99cb3b9345d46417aa04f448a20934
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2021-02-18 02:42:45 +0300

    Minor changes

--- app/src/main/java/com/vickikbt/fixitapp/di/DarajaModule.kt
+++ app/src/main/java/com/vickikbt/fixitapp/di/DarajaModule.kt
@@ -1,12 +1,15 @@
 package com.vickikbt.fixitapp.di
 
+import com.androidstudy.daraja.Daraja
+
+
 //@InstallIn(ActivityRetainedComponent::class)
 
 //@Module
 object DarajaModule {
 
     fun providesDaraja() {
-        //return Daraja.
+        //return Daraja.Builder()
     }
 
 }
